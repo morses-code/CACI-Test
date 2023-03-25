37f044038b47199ebd2796854c1518fcac79c24c
morses-code <morsescott88@gmail.com>
2023-03-25 11:53:42 +0000

    project initialisation

---
+++ src/main/java/com/github/morsescode/brickorderingservice/BrickOrderingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.morsescode.brickorderingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickOrderingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickOrderingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/morsescode/brickorderingservice/BrickOrderingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.morsescode.brickorderingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickOrderingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
